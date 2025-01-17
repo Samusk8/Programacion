285bfcfffd00d1d4c7f48ac41b426a84149f0288
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-01-17 11:50:45 +0100

    array task2

--- SPPROTema3/src/ejerciciosArrays/Task2.java
+++ SPPROTema3/src/ejerciciosArrays/Task2.java
@@ -1,11 +1,20 @@
 package ejerciciosArrays;
 
 public class Task2 {
-    public static int[] sumaValores(int[] array){
+    public static int sumaValores(int[] array){
         int aux;
+        int resultado = 0;
         for (int i = 0; i < array.length; i++) {
-
-            aux = i;
+            array[i]=i+1;
+            resultado = resultado + array[i];
         }
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        int resultado = sumaValores(array);
+        System.out.println(resultado);
+
     }
 }
