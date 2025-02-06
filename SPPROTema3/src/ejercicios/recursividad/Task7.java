79409a4cc4b006ab1adc2fe75ebb98062b2dc91a
Samuel <samueljimenez@paucasesnovescifp.cat>
2025-02-06 18:10:17 +0100

    algo

--- SPPROTema3/src/ejercicios/recursividad/Task7.java
+++ SPPROTema3/src/ejercicios/recursividad/Task7.java
@@ -11,7 +11,7 @@ public class Task7 {
     }
 
     public static void main(String[] args) {
-        int numero1 = 80 ;
+        int numero1 = 20 ;
         int numero2 = 45;
         int resultado = mcd(numero1, numero2);
         System.out.println(resultado);
